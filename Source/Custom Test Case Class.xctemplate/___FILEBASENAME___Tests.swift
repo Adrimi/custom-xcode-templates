d0ed4e1b512931af73774b66ef58412053317ca0
Adrian Szymanowski <adriano100417@gmail.com>
2021-02-09 18:03:11 +0100

    Add  template for creating fast TDD file with ready-to-write code structure

---
+++ Source/Custom Test Case Class.xctemplate/___FILEBASENAME___Tests.swift
@@ -0,0 +1,24 @@
+//
+// Copyright © 2021 ___FULLUSERNAME___. All rights reserved.
+//
+
+import XCTest
+
+class ___VARIABLE_ModuleName___ {
+    
+}
+
+class ___VARIABLE_ModuleName___Tests: XCTestCase {
+
+    func test_init_<#Name your first test!#>() {
+        let sut = makeSUT()
+    }
+    
+    // MARK: - Helpers
+
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> ___VARIABLE_ModuleName___ {
+        let sut = ___VARIABLE_ModuleName___()
+        return sut
+    }
+
+}
